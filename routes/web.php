6a132c6f78e46f2419ea9b5386f0ddae7c5da420
Jamal077011 <jaime077011@gmail.com>
2023-12-31 15:04:49 +0200

    new repo

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| be assigned to the "web" middleware group. Make something great2!
 |
 */
 
